fc629f1b8f538817005223c85bec45b3fb492521
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-31 02:28:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -89,7 +89,17 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-binding.pry
+  array_movies = []
+  row_index = 0
+
+  while row_index < source.length do
+
+    
+    row_index += 1
+  end
+  
+  
+
 end
 
 # ----------------    End of Your Code Region --------------------
