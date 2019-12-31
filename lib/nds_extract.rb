af3d39a32fe181ced5d6b805844f4ca7713e8adf
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-31 02:20:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -56,6 +56,7 @@ def movies_with_director_key(name, movies_collection)
     array_director << movie_with_director_name(name, movie_data)
     row_index += 1
   end
+  puts array_director
   array_director
 end
 
